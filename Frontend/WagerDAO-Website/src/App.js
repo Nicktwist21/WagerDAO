f42af0549fe5068ab3e34a8b09e991098d90e134
Nick Calhoun <87304511+Nicktwist21@users.noreply.github.com>
2022-12-05 20:32:05 +0000

    -Removed ChainlinkOracle contracts -Added api folder -Installed Axios via Node -Added Axios to App.js

--- Frontend/WagerDAO-Website/src/App.js
+++ Frontend/WagerDAO-Website/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 import { NFTContextProvider } from "./context/NFTContext";
 import { XrplContextProvider } from "./context/XrplContext";
---
+++ Frontend/WagerDAO-Website/src/api/Bets.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+
+export default async function handler(req, res) {
+const options = {
+  method: 'GET',
+  url: 'https://api-football-v1.p.rapidapi.com/v3/odds/bets',
+  params: {id: ''},
+  headers: {
+    'X-RapidAPI-Key': '9936e5f73bmsh8711a5d33e127adp15aebajsn8d7d680d2f17',
+    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+  }
+}
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
